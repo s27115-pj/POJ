4c0904ee5923c2727dab06ea29378eac7ff712a2
s27115-pj <s27115@pjwstk.edu.pl>
2023-03-23 20:34:12 +0100

    add zad1

--- java_zad_02/java_02/src/Main.java
+++ java_zad_02/java_02/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+    private String firstName;
+    private String lastName;
+    private String group;
+    private String major;
+
+    public Main(String firstName, String lastName, String group, String major) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.major = major;
+    }
+
+    public void displayInfo() {
+        System.out.println("Imię: " + firstName);
+        System.out.println("Nazwisko: " + lastName);
+        System.out.println("Grupa: " + group);
+        System.out.println("Kierunek studiów: " + major);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main student = new Main("Jan", "Kowalski", "A1", "Informatyka");
+        student.displayInfo();
     }
 }
