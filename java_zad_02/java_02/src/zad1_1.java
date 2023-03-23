de1c5a888692f16b31d7391c2a0464a4fff09e71
s27115-pj <s27115@pjwstk.edu.pl>
2023-03-23 21:49:26 +0100

    add zad1

---
+++ java_zad_02/java_02/src/zad1_1.java
@@ -0,0 +1,29 @@
+public class zad1_1 {
+
+    public static class Student {
+        private String firstName;
+        private String lastName;
+        private int group;
+        private String major;
+
+        public Student(String firstName, String lastName, int group, String major) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.group = group;
+            this.major = major;
+        }
+
+        public void displayInfo() {
+            System.out.println("Imię: " + firstName);
+            System.out.println("Nazwisko: " + lastName);
+            System.out.println("Grupa: " + group);
+            System.out.println("Kierunek studiów: " + major);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Jan", "Kowalski", 1, "Informatyka");
+        student.displayInfo();
+    }
+}
