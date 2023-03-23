179a0ecefcdc63ec80c96f7842b6eea3cff10fcd
s27115-pj <s27115@pjwstk.edu.pl>
2023-03-23 22:12:19 +0100

    add zad3

---
+++ java_zad_02/java_02/src/zad2_3.java
@@ -0,0 +1,24 @@
+
+    public class zad2_3 {
+        static boolean bool;
+        static byte b;
+        static char c;
+        static short s;
+        static int i;
+        static long l;
+        static float f;
+        static double d;
+
+        public static void main(String[] args) {
+            System.out.println("Domyślne wartości dla typów prostych:");
+            System.out.println("boolean: " + bool);
+            System.out.println("byte: " + b);
+            System.out.println("char: " + c);
+            System.out.println("short: " + s);
+            System.out.println("int: " + i);
+            System.out.println("long: " + l);
+            System.out.println("float: " + f);
+            System.out.println("double: " + d);
+        }
+    }
+
